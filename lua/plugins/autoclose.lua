8b34105db56a046ef65646fa8ad5a095a2ce545b
MehrJonas <jonasmehr97@gmail.com>
2024-10-22 21:29:21 +0200

    configure autotag

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,13 +1,19 @@
 return {
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {}
-  },
-  {
-    "windwp/nvim-ts-autotag",
-    init = function()
-        require("nvim-ts-autotag").setup()
-    end
-  }
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        init = function()
+            require("nvim-ts-autotag").setup({
+                opts = {
+                    enable_close = true,
+                    enable_rename = true,
+                    enable_close_on_slash = true,
+                },
+            })
+        end,
+    },
 }
