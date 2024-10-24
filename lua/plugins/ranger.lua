13920b311d5fb2610369ac0cc589c7ffa18e1264
MehrJonas <jonasmehr97@gmail.com>
2024-10-25 01:10:04 +0200

    ranger window size

--- lua/plugins/ranger.lua
+++ lua/plugins/ranger.lua
@@ -1,12 +1,19 @@
 return {
-  "kelly-lin/ranger.nvim",
-  config = function()
-    require("ranger-nvim").setup({ replace_netrw = true })
-    vim.api.nvim_set_keymap("n", "<leader>rr", "", {
-      noremap = true,
-      callback = function()
-        require("ranger-nvim").open(true)
-      end,
-    })
-  end,
+    "kelly-lin/ranger.nvim",
+    config = function()
+        require("ranger-nvim").setup({
+            replace_netrw = true,
+            ui = {
+                height = 0.8,
+                width = 0.8,
+                border = "single",
+            },
+        })
+        vim.api.nvim_set_keymap("n", "<leader>rr", "", {
+            noremap = true,
+            callback = function()
+                require("ranger-nvim").open(true)
+            end,
+        })
+    end,
 }
